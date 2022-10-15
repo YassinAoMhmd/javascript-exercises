35bd8a589ef9d0652c636cdc2d267ef291016691
YassinAoMhmd <113366829+YassinAoMhmd@users.noreply.github.com>
2022-10-16 00:47:43 +0200

    Update findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+	let mayor = 0;
+  let oldest = [];
+	
+	people.reduce((ant, desp) => {
+	
+	if ((desp.yearOfDeath - desp.yearOfBirth) > mayor) {
+   		mayor = desp.yearOfDeath - desp.yearOfBirth;
+     	 	oldest = desp;
+    	}
+    	
+    	return ant;
+  	
+  	})
+  
+  	return oldest;	
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
