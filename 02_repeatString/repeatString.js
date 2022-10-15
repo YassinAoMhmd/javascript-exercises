783eb28235da6fefb2d1ebceec40a8a93468c90f
YassinAoMhmd <113366829+YassinAoMhmd@users.noreply.github.com>
2022-10-15 21:51:31 +0200

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
 const repeatString = function() {
+ let char = 'hey';
+ let result = '';
+ 
+ for(let i=0; i < 3; i++){
+ 	result += char;
+ }
+ 
+ return result;
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
