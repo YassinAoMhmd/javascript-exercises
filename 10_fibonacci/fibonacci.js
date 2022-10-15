e2ac1602291b870f505a5122e8cfc585d6a8d7aa
YassinAoMhmd <113366829+YassinAoMhmd@users.noreply.github.com>
2022-10-16 00:19:27 +0200

    Update fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+	let aux = 0;
+	let x = 0;
+  	let y = 1;
 
+  	if (number == 0) {
+  		return 0;
+  	}	
+  
+  	for (let i = 1; i < number; i++) {
+    		aux = y;
+    
+    		y = x + y;
+    		x = aux;
+  	}
+  	
+  	return y;
 };
 
 // Do not edit below this line
