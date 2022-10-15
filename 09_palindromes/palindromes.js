e274bd7e78da8183cba7577c616a3cf2b9f1b414
YassinAoMhmd <113366829+YassinAoMhmd@users.noreply.github.com>
2022-10-16 00:07:00 +0200

    Update palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	let equal = 0;
+	let aux = 0;
+	
+	for(let i = (string.length - 1); i >= 0; i--) {
+	        if(string[i] == string[aux]) {
+	            equal++;
+	        }
+	        aux++;
+	}
+	    
+	if(string.length == equal) {
+	     return true;
+	} else {
+	     return false;
+	}
 };
 
 // Do not edit below this line
