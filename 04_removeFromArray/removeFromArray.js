c5e24624e457b9a4ea65158a992da684b7a35807
YassinAoMhmd <113366829+YassinAoMhmd@users.noreply.github.com>
2022-10-15 22:56:31 +0200

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(cars,position) {
+	const result = [];
+	let j=0;
+	
+	for(let i=0; i < cars.length; i++){
+		if(i != (position-1)){
+			result[j] = cars[i];
+			j++;	
+		}
+	}
+	
+	return result;
 
 };
 
