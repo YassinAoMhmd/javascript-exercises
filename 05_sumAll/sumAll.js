bbc1d5a1d29b9f403ddf1b69e9db2dbf74a0573a
YassinAoMhmd <113366829+YassinAoMhmd@users.noreply.github.com>
2022-10-15 23:06:44 +0200

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(left, right) {
+	let sum = 0;
+	
+	for(let i = left; i <= right; i++){
+		sum += i;
+	}
+	
+	return sum;
 };
 
 // Do not edit below this line
