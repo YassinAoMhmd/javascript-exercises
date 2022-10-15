64ebf4c4075517199f2b5f377ebec2148246b913
YassinAoMhmd <113366829+YassinAoMhmd@users.noreply.github.com>
2022-10-15 23:30:58 +0200

    Update tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(number) {
+	let celsius = 0;
+	
+	celsius = ((number-32) * (5/9));
+	
+	let rounded = Math.round(celsius * 10) / 10;
+	
+	return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(number) {
+	let fahrenheit = 0;
+	
+	fahrenheit = (number * (9/5) + 32);
+	
+	let rounded = Math.round(fahrenheit * 10) / 10;
+	
+	return rounded;
 };
 
 // Do not edit below this line
