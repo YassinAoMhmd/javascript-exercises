18e99f8cb19ab1632d94f57edd57c1e0a46a3505
YassinAoMhmd <113366829+YassinAoMhmd@users.noreply.github.com>
2022-10-15 23:57:01 +0200

    Update calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,53 @@
-const add = function() {
+const add = function(number1, number2) {
+	let sum = number1 + number2;
 	
+	return sum;	
 };
 
-const subtract = function() {
+const subtract = function(number1, number2) {
+	let resta = number1 - number2;
 	
+	return resta;
 };
 
-const sum = function() {
+const sum = function(array) {
+	let sum = 0;
 	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
+	for(let i=0; i < array.length; i++){
+		sum += array[i];
+	}
 	
+	return sum;		
 };
 
-const factorial = function() {
+const multiply = function(array) {
+	let mult = 1;
+	
+	for(let i=0; i < array.length; i++){
+		mult *= array[i];
+	}
+	
+	return mult;	
+};
+
+const power = function(number1, number2) {
+	let pow =1;
+	
+	for(let i = 0; i < number2; i++){
+		pow *= number1;
+	}
+	
+	return pow;	
+};
+
+const factorial = function(number) {
+	let fact = 1;
+	
+	for(let i = 1; i <= number; i++){
+		fact *= i;
+	}
+	
+	return fact;	
 	
 };
 
